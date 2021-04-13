479320f7f6ba579f3f7cf1d40a8cb6ca19f8a4d4
Olivier <>
2021-04-13 10:48:10 +0200

    TP 5

--- js/form.js
+++ js/form.js
@@ -1,5 +1,3 @@
-let clients = [];
-
 function displayClient() {
   let nom = document.getElementById('name').value;
   let prenom = document.getElementById('firstname').value;
@@ -16,8 +14,8 @@ function displayClient() {
       && password.length != 0
       && password.length <= 6
       && age != ""
-      ) {
-    let newClient = new Client (nom, prenom, password, civ, status, age);
+      ) { 
+    let newClient = new Client (nom, prenom, password, civ, status, parseInt(age));
     clients.push(newClient);
     message = "Félicitations, l'ajout du client à fonctionner";
     document.getElementById('message').style.color = "green";
@@ -34,7 +32,6 @@ function displayClient() {
   }
   document.getElementById('message').innerHTML = message;
 }
-
 
 function controles(nom, prenom, age, password) {
   let message = "Erreurs sur les champs suivants: ";
@@ -57,6 +54,38 @@ function controles(nom, prenom, age, password) {
   return message;
 }
 
+function choiceUser() {
+  let action = document.getElementById('info').value;
+  let recherche = document.getElementById('text-search').value;
+
+  // IF Search
+  if (action == 'search') {
+    let found = false;
+    for (i=0; i < clients.length; i++) {
+      if (recherche == clients[i].nom) {
+        found = true;
+        document.getElementById('message2').innerHTML = clients[i].civilite + ' ' + clients[i].nom.toUpperCase() + ' ' +
+          clients[i].prenom + "(" + clients[i].age + " ans)";
+        break;
+      }
+    }
+    if (!found) {
+      document.getElementById('message2').innerHTML = "Désolé, le client n'est pas répertorié";
+    }
+  }
+
+
+  // IF Avairage
+  else if (action == 'avairage') {
+    let avairage = 0;
+    for (i in clients) {
+      avairage += clients[i].age;
+    }
+    avairage = avairage/clients.length;
+    document.getElementById('message2').innerHTML = "Moyenne d'age des clients: " + avairage;
+  }
+}
+
 
 
 
@@ -71,3 +100,7 @@ class Client {
   }
 }
 
+let clients = [
+  new Client('TEST', 'Jean', 'ABCD', 'Monsieur', true, 25 ), 
+  new Client('TEST2', 'Edouard', 'ABCD', 'Madame', true, 35 )
+];
